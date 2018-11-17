dac16a42e8c85691f588eb4ba596d12a56ebcb8c
Chu <Chuoguejiofor@gmail.com>
2018-11-16 20:00:41 -0800

    Added favorite artist chart

--- app.R
+++ app.R
@@ -8,11 +8,15 @@ ui <- dashboardPage(
   dashboardBody(
     # Boxes need to be put in a row (or column)
     fluidRow(
-      box(withSpinner(plotOutput("plot1", height = 250))),
-      
       box(
         title = "Enter username",
-        textInput("username", "Username", value="chu23")
+        textInput("username", "Username", value="chu23"),
+        submitButton(text = "Submit")
+      ),
+      
+      box(
+        title = "Favorite Artists",
+        withSpinner(plotOutput("plot1", height = 250))
       )
     )
   )
@@ -20,11 +24,15 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
   library(spotifyr)
+  library(dplyr)
+  library(ggplot2)
   access_token <- get_spotify_access_token()
-  
+
   output$plot1 <- renderPlot({
     user <- get_user_audio_features(input$username)
-    hist(user$danceability)
+    top_artists <- user %>% group_by(artist_name) %>% summarise(count = n()) %>% arrange(desc(count))
+    g <- ggplot(head(top_artists), aes(artist_name, count))
+    g + geom_bar(stat='identity') + theme(axis.text.x=element_text(angle=50, hjust=1))
   })
 }
 
